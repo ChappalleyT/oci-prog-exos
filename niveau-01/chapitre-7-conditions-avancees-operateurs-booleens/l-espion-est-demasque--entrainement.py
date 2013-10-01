9ce3192f1e1d03d6c17904bba43edd7f462356e8
ChappalleyT <ChappalleyT@gmail.com>
2013-10-01 15:28:17 +0200

    Update l-espion-est-demasque--entrainement.py

--- niveau-01/chapitre-7-conditions-avancees-operateurs-booleens/l-espion-est-demasque--entrainement.py
+++ niveau-01/chapitre-7-conditions-avancees-operateurs-booleens/l-espion-est-demasque--entrainement.py
@@ -1,4 +1,3 @@
-
 ##################################
 # fichier l-espion-est-demasque--entrainement.py
 # nom de l'exercice :  L'espion est démasqué !
@@ -17,3 +16,29 @@
 
 # mettre votre code ici
 
+nbPersonnes = int(input())
+nombre = 0
+for loop in range (nbPersonnes):
+   taille = int(input())
+   age = int(input())
+   poid = int(input())
+   cheval = int(input())
+   cheveux = int(input())
+   if (taille>=178) and (taille<=182):
+      nombre = nombre+1
+   if age>=34:
+      nombre = nombre+1
+   if poid < 70:
+      nombre = nombre+1
+   if cheval == 0:
+      nombre = nombre+1   
+   if cheveux == 1:
+      nombre = nombre+1
+   if nombre == 5:
+      print("Très probable")
+   elif (nombre==4) or (nombre==3):
+      print("Probable")
+   elif  nombre==0:
+      print("Impossible")
+   elif (nombre==1) or (nombre==2):
+      print("Peu probable")
