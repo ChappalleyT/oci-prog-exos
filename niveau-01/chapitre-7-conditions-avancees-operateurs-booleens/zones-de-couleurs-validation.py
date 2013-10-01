38a6295117f40b129c43f5d7494c60a777199b35
ChappalleyT <ChappalleyT@gmail.com>
2013-10-01 15:28:59 +0200

    Update zones-de-couleurs-validation.py

--- niveau-01/chapitre-7-conditions-avancees-operateurs-booleens/zones-de-couleurs-validation.py
+++ niveau-01/chapitre-7-conditions-avancees-operateurs-booleens/zones-de-couleurs-validation.py
@@ -1,4 +1,3 @@
-
 ##################################
 # fichier zones-de-couleurs-validation.py
 # nom de l'exercice :  Zones de couleurs
@@ -16,4 +15,17 @@
 
 
 # mettre votre code ici
+
+N = int(input())
+for loop in range (N):
+   X = int(input())
+   Y = int(input())
+   if (X>15) and (X<45) and (Y>60) and (Y<70) or (X>60) and (X<85) and (Y>60) and (Y<70):
+      print("Dans une zone rouge")
+   if (X>10) and (X<25) and (Y>10) and (Y<55) or (X>25) and (X<50) and (Y>10) and (Y<20) or (X>25) and (X<50)and (Y>45) and (Y<55) or (X>50) and (X<85) and (Y>10) and (Y<55):
+      print("Dans une zone bleue")
+   if (X>0) and (X<90) and (Y>0) and (Y<10) or (X>0) and (X<10) and (Y>10) and (Y<55) or (X>0) and (X<15) and (Y>55) and (Y<70) or (X>85) and (X<90) and (Y>0) and (Y<70) or (X>15) and (X<85) and (Y>55) and (Y<60) or (X>45) and (X<60 and (Y>60) and (Y<70)or (X>25) and(X<50) and (Y>20) and (Y<45):
+      print("Dans une zone jaune")
+   else:
+      print("En dehors de la feuille")
 
