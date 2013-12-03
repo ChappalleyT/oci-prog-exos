52caebe6a90552f5a0bceface91269bb30143f2e
ChappalleyT <ChappalleyT@gmail.com>
2013-12-03 16:21:17 +0100

    Update 08-visite-de-la-mine-obligatoire.py

--- niveau-02/chapitre-2-decouverte-tableaux/08-visite-de-la-mine-obligatoire.py
+++ niveau-02/chapitre-2-decouverte-tableaux/08-visite-de-la-mine-obligatoire.py
@@ -1,4 +1,3 @@
-
 ##################################
 # fichier 08-visite-de-la-mine-obligatoire.py
 # nom de l'exercice : Visite de la mine
@@ -17,3 +16,14 @@
 
 # mettre votre code ici
 
+deplacInverse = [0, 2, 1, 3, 5, 4]
+ 
+nbDeplac = int(input())
+chemin = [0] * nbDeplac
+ 
+for num in range(nbDeplac):
+   chemin[num] = int(input())
+ 
+for num in range(nbDeplac-1, -1, -1):
+   deplac = chemin[num]
+   print(deplacInverse[deplac])
