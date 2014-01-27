04d427dd528cecb423177d1189f6f10a6c0c8447
ChappalleyT <ChappalleyT@gmail.com>
2014-01-27 19:29:08 +0100

    Update 04g-la-bataille-obligatoire.py

--- niveau-02/chapitre-3-chaines-de-caracteres/04g-la-bataille-obligatoire.py
+++ niveau-02/chapitre-3-chaines-de-caracteres/04g-la-bataille-obligatoire.py
@@ -1,4 +1,3 @@
-
 ##################################
 # fichier 04g-la-bataille-obligatoire.py
 # nom de l'exercice : La bataille
@@ -16,4 +15,29 @@
 
 
 # mettre votre code ici
+paquet1 = input()
+paquet2 = input()
+gagnantTrouve = False
+nbEgales = 0
+indice = 0
+while not gagnantTrouve and indice < min(len(paquet1), len(paquet2)):
+   if paquet1[indice] != paquet2[indice]:
+      gagnantTrouve = True
+      if paquet1[indice] < paquet2[indice]:
+         print(1)
+      elif paquet1[indice] > paquet2[indice]:
+         print(2)
+   else:
+      nbEgales = nbEgales + 1
+   indice = indice + 1  
+    
+if not gagnantTrouve:
+   if len(paquet1) > len(paquet2):
+      print(1)
+   elif len(paquet1) < len(paquet2):
+      print(2)
+   else:
+      print("=")
+ 
+print(nbEgales)
 
